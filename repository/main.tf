55e8f0047bd63d7d7d4c4a95d3bbf466ce8b4f4c
Kazik Pogoda <morisil@xemantic.com>
2023-09-23 01:10:00 +0200

    repo id fixed

--- repository/main.tf
+++ repository/main.tf
@@ -30,13 +30,15 @@ resource github_repository this {
 }
 
 resource github_branch_protection main {
-  repository_id                   = github_repository.this.id
+  repository_id                   = github_repository.this.node_id
   pattern                         = "main"
   allows_deletions                = false
   allows_force_pushes             = false
   blocks_creations                = false
   enforce_admins                  = false
+  force_push_bypassers            = []
   lock_branch                     = false
+  push_restrictions               = []
   require_conversation_resolution = false
   require_signed_commits          = false
   required_linear_history         = false
