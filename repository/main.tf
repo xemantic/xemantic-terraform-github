e932133f75c24c27aceea14a9d76f0f48a1903cb
Kazik Pogoda <morisil@xemantic.com>
2023-09-22 13:57:32 +0200

    no wiki by default

--- repository/main.tf
+++ repository/main.tf
@@ -15,6 +15,7 @@ resource github_repository this {
   name = var.repository
   description = var.description
   vulnerability_alerts = true
+  has_wiki = false
   dynamic "pages" {
     for_each = var.pages_url != null ? [1] : []
     content {
