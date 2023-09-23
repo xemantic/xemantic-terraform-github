49ae74835b5428f838c1fb4cad265987e26f9061
Kazik Pogoda <morisil@xemantic.com>
2023-09-23 20:48:42 +0200

    misformatted file to test github action

--- repository/variables.tf
+++ repository/variables.tf
@@ -19,8 +19,25 @@ variable "private" {
   default = false
 }
 
+variable "homepage_url" {
+  type = string
+  description = "URL of a page describing the project."
+  default = null
+}
+
 variable "pages_url" {
   description = "CNAME for GitHub pages"
   type        = string
   default     = null
 }
+
+variable "has_downloads" {
+  type = bool
+  default = false
+}
+
+variable "has_issues" {
+  type = bool
+  default = false
+}
+
